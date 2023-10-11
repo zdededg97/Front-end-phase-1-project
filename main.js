ee1f6b1d05cfb37a1b7797cae0f8cff9325303de
Edgar Hernandez <zdededg@gmail.com>
2023-10-10 21:28:18 -0700

    Add and query api

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('connected')
+console.log("main.js connected");
+
+const searchTermsInput = document.body.querySelector("#search-terms");
+
+const getPokeCategories = async () => {
+  const PokeCatergoriesApiURL = "https://pokeapi.co/api/v2/ability/150/";
+
+  try {
+    const response = await fetch(PokeCatergoriesApiURL);
+    const data = response.json();
+    console.log("data: ", data);
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong, try again later");
+  }
+};
+
+const handleFormInputFocus = async () => {
+  console.log("focus occurred");
+
+  await getPokeCategories();
+};
+
+searchTermsInput.addEventListener("focus", handleFormInputFocus);

16fac08b272809e721bf1c37affc78053c6d5a20
Edgar Hernandez <zdededg@gmail.com>
2023-10-11 21:12:10 -0700

    update add querry error

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('connected')
+console.log("main.js connected");
+
+const searchTermsInput = document.body.querySelector("#search-terms");
+
+const getPokeCategories = async () => {
+  const PokeCategoriesApiURL = "https://pokeapi.co/api/v2/ability/150/";
+
+  try {
+    const response = await fetch(PokeCategoriesApiURL);
+    const data = await response.json();
+    console.log("data: ", data);
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong, try again later");
+  }
+};
+
+const handleFormInputFocus = async () => {
+  console.log("focus occurred");
+
+  await getPokeCategories();
+};
+
+searchTermsInput.addEventListener("focus", handleFormInputFocus);
